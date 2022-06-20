c264ec4fb65be6c6ae9bfa1649f45fbd5df38d31
theodorepk <theodore.perron.kyritsos@gmail.com>
2022-06-20 22:19:40 +0200

    js for the modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log(`The trip begins...`);
+
+  document.querySelector(".log-in").addEventListener("click", () => {
+    document.querySelector(".modal").classList.remove("hidden");
+  });
+
+  document.querySelector("#mark").addEventListener("click", () => {
+    document.querySelector(".modal").classList.add("hidden");
+  });
+
+  document
+    .getElementById("contactForm")
+    .addEventListener("submit", async (event) => {
+      event.preventDefault();
+
+      const response = await axios.post(
+        "https://tpk-formulaire-back-end.herokuapp.com/form",
+        {
+          firstname: document.querySelector("#firstname").value,
+          lastname: document.querySelector("#lastname").value,
+          email: document.querySelector("#email").value,
+          subject: document.querySelector("#subject").value,
+          message: document.querySelector("#message").value,
+        }
+      );
+    });
 });
